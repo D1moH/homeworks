dc4bdb8b68c556cb4b1f12018806d1623d724df7
D1moH <21DIMON12@inbox.ru>
2022-02-08 03:08:44 +0300

    Добавляет ДЗ

--- homeworks-js-03/js/task-3.js
+++ homeworks-js-03/js/task-3.js
@@ -1,3 +1,36 @@
 'use strict'
 
+const findBestEmployee = function (employees) {
+    let value = 0;
+    let name;
+    for (const employee in employees) {
+        if (value <= employees[employee]) {
+            value = employees[employee];
+            name = employee;
+        }
+    }
+    return name;
+}
 
+console.log(
+    findBestEmployee({
+        ann: 29,
+        david: 35,
+        helen: 1,
+        lorence: 99,
+}));
+
+console.log(
+    findBestEmployee({
+        poly: 12,
+        mango: 17,
+        ajax: 4,
+}));
+
+console.log(
+    findBestEmployee({
+        lux: 147,
+        david: 21,
+        kiwi: 19,
+        chelsy: 38,
+}));
