647af09c81282d62577da9035710e7e284c9785e
D1moH <21DIMON12@inbox.ru>
2022-02-24 13:11:26 +0300

    add

---
+++ homeworks-js-08/js/main.js
@@ -0,0 +1,97 @@
+import galleryItems from "/homeworks-js-08/js/gallery-items.js";
+
+const refs = {
+    gallery: document.querySelector('.gallery'),
+    lightbox: document.querySelector('.lightbox'),
+    overlay: document.querySelector('.lightbox__overlay'),
+    button: document.querySelector('.lightbox__button'),
+    image: document.querySelector('.lightbox__image'),
+    left: document.querySelector('.lightbox__left'),
+    right: document.querySelector('.lightbox__right')
+};
+
+refs.gallery.addEventListener('click', openModal);
+
+const createGallery = () => {
+    const addImg = ({ preview, original, description }, index) =>
+        `<li class="gallery__item">
+         <a class="gallery__link">
+            <img class="gallery__image" src="${preview}"data-source="${original}" alt="${description}" data-index ="${index}"/>      
+       </a>
+   </li>`;
+    const galleryRef = galleryItems
+        .map((item, index) => addImg(item, index))
+        .join('');
+    refs.gallery.insertAdjacentHTML('afterbegin', galleryRef)
+};
+
+function openModal(e) {
+    const target = e.target;
+    if (target.nodeName === 'IMG') {
+        refs.lightbox.classList.add('is-open');
+        change
+    }
+}
+
+createGallery(galleryItems);
+
+// refs.gallery.addEventListener("click", openModal);
+// refs.button.addEventListener("click", closeModal);
+// refs.leftArrow.addEventListener("click", changeImg);
+// refs.rightArrow.addEventListener("click", changeImg);
+// refs.lightboxOverlay.addEventListener("click", closeModal);
+// window.addEventListener("keydown", (event) =>
+//   event.code === "Escape" ? closeModal() : null
+// );
+// window.addEventListener("keydown", changeImg);
+
+// let currentImg;
+
+
+// function changeAttribute(src, alt) {
+//   refs.lightboxImg.src = src;
+//   refs.lightboxImg.alt = alt;
+// }
+
+// function openModal(event) {
+//   const target = event.target;
+
+//   if (target.nodeName === "IMG") {
+//     refs.lightbox.classList.add("is-open");
+//     changeAttribute(target.dataset.source, target.alt);
+//     // refs.lightboxImg.src = target.dataset.source;
+//     // refs.lightboxImg.alt = target.alt;
+//     refs.lightboxImg.dataset.index = target.dataset.index;
+//     currentImg = +target.dataset.index;
+//     return;
+//   }
+// }
+
+// function closeModal() {
+//   refs.lightbox.classList.remove("is-open");
+//   changeAttribute("", "");
+//   // refs.lightboxImg.src = "";
+//   // refs.lightboxImg.alt = "";
+//   return;
+// }
+
+// function changeImg(event) {
+//   let len = galleryItems.length - 1;
+
+//   if (
+//     event.target.classList.value.includes("left") ||
+//     event.code === "ArrowLeft"
+//   ) {
+//     currentImg === 0 ? (currentImg = len) : (currentImg -= 1);
+//     refs.lightboxImg.src = galleryItems[currentImg].original;
+//   }
+//   if (
+//     event.target.classList.value.includes("right") ||
+//     event.code === "ArrowRight"
+//   ) {
+//     currentImg === len ? (currentImg = 0) : (currentImg += 1);
+//     refs.lightboxImg.src = galleryItems[currentImg].original;
+//   }
+// }
+
+// createGallery(galleryItems);
