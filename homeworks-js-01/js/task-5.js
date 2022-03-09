ed823556895ef97218eb0f49748165e4e4cd4449
D1moH <21DIMON12@inbox.ru>
2022-03-09 19:29:22 +0300

    Update task-5.js

--- homeworks-js-01/js/task-5.js
+++ homeworks-js-01/js/task-5.js
@@ -5,49 +5,39 @@ let price;
 let message = "Отменено пользователем";
 let isValid = true;
 
-let userInput = prompt("В какую страну Вы бы хотели осуществить доставку?");
-if (userInput !== null) {
-  switch (userInput.toLowerCase()) {
+let input = prompt("В какую страну Вы бы хотели бы осуществить доставку ?");
+if (input === null) {
+    console.log(message);
+    alert(message);
+}
+switch (input.toLowerCase()) {
     case "китай":
       country = "Китай";
       price = 100;
-      console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
-
     case "чили":
       country = "Чили";
       price = 250;
-      console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
-
     case "австралия":
       country = "Австралию";
       price = 170;
-      console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
-
     case "индия":
       country = "Индию";
       price = 80;
-      console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
-
     case "ямайка":
       country = "Ямайку";
       price = 120;
-      console.log(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
-
+      
     default:
-      isValid = false;
-      message = "В вашей стране доставка не доступна";
+        isValid = false;
+        message = "В вашей стране доставка не доступна";
   }
-} else {
-  isValid = false;
-}
-
 if (isValid) {
   message = `Доставка в ${country} будет стоить ${price} кредитов`;
 }
-
+console.log(message);
 alert(message);
